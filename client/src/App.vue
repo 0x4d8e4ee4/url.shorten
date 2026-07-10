f9b91784d4b1493d1af5c113d0b57db14426359f
triteit <widelix365@gmail.com>
2026-07-10 16:13:37 +0300

    refactor: update HTML structure and enhance HomeView functionality

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,81 +11,84 @@ const links = [
 </script>
 
 <template>
-  <header class="pt-5 px-10 border-b border-slate-200 flex flex-row justify-between items-end">
-    <span class="font-mono font-semibold tracking-tight text-lg text-slate-900 pb-4">
-      url.shorten()
-    </span>
+  <div class="flex flex-col min-h-screen">
+    <header class="pt-5 px-10 border-b border-slate-200 flex flex-row justify-between items-end">
+      <span class="font-mono font-semibold tracking-tight text-lg text-slate-900 pb-4">
+        url.shorten()
+      </span>
 
-    <div class="flex flex-row font-mono uppercase justify-end font-semibold gap-15">
-      
-      <RouterLink v-for="link in links" :key="link.to" :to="link.to" v-slot="{ isActive }" class="pb-4.5 relative group">
-        <span :class="isActive ? 'text-slate-900' : 'text-slate-400 group-hover:text-slate-700 transition-colors duration-300'">
-          {{ link.label }}
-        </span>
-
-        <div class="absolute bottom-0 left-0 h-0.75 bg-slate-900 transition-all duration-300 origin-center" :class="isActive ? 'w-full scale-x-100' : 'w-full scale-x-0 group-hover:scale-x-100'" />
-      </RouterLink>
-    </div>
-  </header>
-
-  <RouterView />
-
-  <footer class="border-t border-slate-100 bg-white px-10 py-7">
-    <div class="mx-auto grid grid-cols-1 gap-8 sm:grid-cols-3">
-      
-      <!-- Левая колонка: Брендинг -->
-      <div class="flex flex-col justify-between space-y-4 sm:space-y-0">
-        <div>
-          <span class="font-mono text-lg font-semibold tracking-tight text-slate-900 block pb-1">
-            url.shorten()
-          </span>
-          <span class="font-mono text-sm font-medium text-slate-600 leading-relaxed block">
-            Простой сервис для сокращения ссылок
-          </span>
-        </div>
-        <span class="font-mono font-medium text-sm text-slate-600">
-          &copy; 2026 url.shorten()
-        </span>
-      </div>
-
-      <!-- Средняя колонка: Навигация -->
-      <div class="flex flex-col space-y-3 sm:items-center">
-        <div class="flex flex-col space-y-2.5">
-          <a href="#" class="font-mono text-sm font-medium text-slate-600 transition-colors duration-200 hover:text-slate-950">
-            О сервисе
-          </a>
-          <a href="#" class="font-mono text-sm font-medium text-slate-600 transition-colors duration-200 hover:text-slate-950">
-            Как это работает
-          </a>
-          <a href="#" class="font-mono text-sm font-medium text-slate-600 transition-colors duration-200 hover:text-slate-950">
-            API
-          </a>
-          <a href="#" class="font-mono text-sm font-medium text-slate-600 transition-colors duration-200 hover:text-slate-950">
-            Поддержка
-          </a>
-        </div>
-      </div>
-
-      <!-- Правая колонка: Соцсети и копирайт -->
-      <div class="flex flex-col justify-between items-start sm:items-end space-y-10 sm:space-y-0">
-        <!-- Иконки с мягким ховером -->
-        <div class="flex items-center gap-8.5 text-black transition-all duration-200">
-          <a href="#" class="duration-200 hover:scale-90 hover:text-slate-400">
-            <Icon icon="simple-icons:github" width="32" />
-          </a>
-          <a href="#" class="duration-200 hover:text-slate-600 hover:scale-90">
-            <Icon icon="simple-icons:telegram" width="32" />
-          </a>
-          <a href="#" class="duration-200 hover:text-slate-600 hover:scale-90">
-            <Icon icon="lucide:mail" width="32" />
-          </a>
-        </div>
+      <div class="flex flex-row font-mono uppercase justify-end font-semibold gap-15">
         
-        <span class="font-mono text-sm font-medium text-slate-600 text-right">
-          Сделано с любовью <Heart color="black" size="14" fill="black" class="animate-pulse inline mb-1" /><br>в минимализме
-        </span>
-      </div>
+        <RouterLink v-for="link in links" :key="link.to" :to="link.to" v-slot="{ isActive }" class="pb-4.5 relative group">
+          <span :class="isActive ? 'text-slate-900' : 'text-slate-400 group-hover:text-slate-700 transition-colors duration-300'">
+            {{ link.label }}
+          </span>
 
-    </div>
-  </footer>
+          <div class="absolute bottom-0 left-0 h-0.75 bg-slate-900 transition-all duration-300 origin-center" :class="isActive ? 'w-full scale-x-100' : 'w-full scale-x-0 group-hover:scale-x-100'" />
+        </RouterLink>
+      </div>
+    </header>
+
+    <main class="grow">
+      <RouterView />
+    </main>
+
+    <footer class="border-t border-slate-100 bg-white px-10 py-7">
+      <div class="mx-auto grid grid-cols-1 gap-8 sm:grid-cols-3">
+        <div class="flex flex-col justify-between space-y-4 sm:space-y-0">
+          <div>
+            <span class="font-mono text-lg font-semibold tracking-tight text-slate-900 block pb-1">
+              url.shorten()
+            </span>
+
+            <span class="font-mono text-sm font-medium text-slate-600 leading-relaxed block">
+              Простой сервис для сокращения ссылок
+            </span>
+          </div>
+
+          <span class="font-mono font-medium text-sm text-slate-600">
+            &copy; 0x4d8e4854
+          </span>
+        </div>
+
+        <div class="flex flex-col space-y-3 sm:items-center">
+          <div class="flex flex-col space-y-2.5">
+            <a href="#" class="font-mono text-sm font-medium text-slate-600 transition-colors duration-200 hover:text-slate-950">
+              О сервисе
+            </a>
+            <a href="#" class="font-mono text-sm font-medium text-slate-600 transition-colors duration-200 hover:text-slate-950">
+              Как это работает
+            </a>
+            <a href="#" class="font-mono text-sm font-medium text-slate-600 transition-colors duration-200 hover:text-slate-950">
+              API
+            </a>
+            <a href="#" class="font-mono text-sm font-medium text-slate-600 transition-colors duration-200 hover:text-slate-950">
+              Поддержка
+            </a>
+          </div>
+        </div>
+
+        <div class="flex flex-col justify-between items-start sm:items-end space-y-10 sm:space-y-0">
+          <div class="flex items-center gap-8.5 text-black transition-all duration-200">
+            <a href="#" class="duration-200 hover:scale-90 hover:text-slate-400">
+              <Icon icon="simple-icons:github" width="32" />
+            </a>
+
+            <a href="#" class="duration-200 hover:text-slate-600 hover:scale-90">
+              <Icon icon="simple-icons:telegram" width="32" />
+            </a>
+
+            <a href="#" class="duration-200 hover:text-slate-600 hover:scale-90">
+              <Icon icon="lucide:mail" width="32" />
+            </a>
+          </div>
+          
+          <span class="font-mono text-sm font-medium text-slate-600 text-right">
+            Сделано с любовью <Heart color="black" size="14" fill="black" class="animate-pulse inline mb-1" /><br>в минимализме
+          </span>
+        </div>
+
+      </div>
+    </footer>
+  </div>
 </template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { ref } from 'vue'
-import { LinkIcon, ChevronDownIcon, LoaderCircleIcon } from '@lucide/vue'
+import { LinkIcon, ChevronDownIcon, LoaderCircleIcon, QrCodeIcon } from '@lucide/vue'
 import axios from 'axios'
 
 const link = ref('')
 const loading = ref(false)
 const shortLink = ref('')
+const copied = ref(false)
 
 const handleSubmit = async () => {
   const cleanLink = link.value.trim().replace(/^https?:\/\//, '')
@@ -27,6 +28,18 @@ const handleSubmit = async () => {
   }
 }
 
+const copyUrl = async (text) => {
+  try {
+    copied.value = true
+    await navigator.clipboard.writeText(text);
+    setTimeout(() => {
+      copied.value = false
+    }, 3000);
+  } catch (err) {
+    console.error('Не удалось скопировать: ', err);
+  }
+};
+
 </script>
 
 <template>
@@ -38,23 +51,39 @@ const handleSubmit = async () => {
     <span>Превращай нечитабельные ссылки в короткие<br>и отслеживай каждый клик.</span>
   </div>
 
-  <div class="flex flex-row lg:w-240 sm:w-3/4 mx-auto h-16 border mt-10 items-center pl-5">
-    <LinkIcon size="24"/>
+  <div class="flex flex-row lg:w-240 sm:w-3/4 mx-auto h-16 border mt-10 items-center">
+    <div class="flex justify-center items-center h-full px-4.5 border-r">
+      <LinkIcon :stroke-width="1.5" class="w-6 h-6" />
+    </div>
+    
     <input type="text" placeholder="Вставьте длинную ссылку" v-model="link" class="p-5 w-full focus:outline-none font-mono font-medium text-sm placeholder:text-slate-600">
-    <button class="bg-black w-48 h-full uppercase text-white font-mono" @click="handleSubmit">
-      <LoaderCircleIcon v-if="loading" size="20" class="animate-spin inline" />
-      <span v-else>создать</span>
+    
+    <button class="bg-black w-48 border-l h-full uppercase text-white font-mono cursor-pointer hover:bg-white hover:text-black transition-all duration-300" @click="handleSubmit">
+      <LoaderCircleIcon v-if="loading" size="20" class="animate-spin inline text-black transition-all duration-300" />
+      <span v-else class="">создать</span>
     </button>
   </div>
 
-  <div class="flex items-center justify-center lg:w-240 sm:w-3/4 mx-auto mt-10 mb-60">
+  <div class="flex items-center justify-center lg:w-240 sm:w-3/4 mx-auto my-10">
     <div class="grow border-t"></div>
     <span class="ml-4 font-mono text-sm font-semibold text-black uppercase tracking-wider whitespace-nowrap">дополнительные настройки</span>
     <ChevronDownIcon size="16" class="mx-4" />
     <div class="grow border-t"></div>
   </div>
 
-  <div v-if="shortLink" class="bg-green-300/50 text-white font-medium font-mono">
-    <span>{{ shortLink }}</span>
+  <div class="flex flex-row lg:w-240 sm:w-3/4 mx-auto h-16 border mt-10 items-center">
+    <div class="flex cursor-pointer justify-center items-center h-full px-4.5 border-r transition-all duration-300 hover:bg-black hover:text-white">
+      <QrCodeIcon :stroke-width="1.3" class="w-6 h-6" />
+    </div>
+    
+    <span class="p-5 w-full focus:outline-none font-mono font-medium text-sm placeholder:text-slate-600">civacel.io/shorturl</span>
+    
+    <button v-if="copied" class="w-48 border-l border-black h-full uppercase text-black font-mono" :disabled="copied">
+      <span>скопировано</span>
+    </button>
+
+    <button v-else class="w-48 border-l border-black cursor-pointer h-full uppercase text-black font-mono hover:bg-black hover:text-white transition-all duration-300" @click="copyUrl('https://civacel.io/shorturl')">
+      <span>копировать</span>
+    </button>
   </div>
 </template>
